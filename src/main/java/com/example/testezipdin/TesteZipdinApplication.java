75db07fa37c8f095e42782f24dc28114e1102152
Franklin Aguiar Sousa <franklin.sousa@basisti.com.br>
2025-06-11 16:48:42 -0300

    first commit

---
+++ src/main/java/com/example/testezipdin/TesteZipdinApplication.java
@@ -0,0 +1,13 @@
+package com.example.testezipdin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteZipdinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteZipdinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testezipdin/TesteZipdinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testezipdin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteZipdinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
